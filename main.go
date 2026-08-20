47df89c466b97ede9a672da7b2cdff839d697eb8
Yoshihiro Koyama <me@yox5ro.net>
2026-08-20 23:44:49 +0900

    added welcome message

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
+func printInitialMsg(port int) {
+	str := `
+    __  ___           __    _ 
+   /  |/  /___  _____/ /_  (_)
+  / /|_/ / __ \/ ___/ __ \/ / 
+ / /  / / /_/ / /__/ / / / /  
+/_/  /_/\____/\___/_/ /_/_/ 
+
+Mochi server started: http://localhost:%d
+`
+
+	fmt.Printf(str, port)
+}
+
 func main() {
-	port := flag.String("port", ":8080", "port to use when protocol is HTTP")
+	port := flag.Int("port", 8080, "port to use")
 	flag.Parse()
 
 	store := InMemoryMapStore{}
 
+	printInitialMsg(*port)
 	s := newHTTPServer(store)
 	s.serveHTTP(*port)
 }
